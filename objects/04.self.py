0a3569a0d28a5af541b7123e873502378ac4517d
Muga <nmugasaez@gmail.com>
2022-09-26 11:01:58 -0300

    first commit, objects

--- objects/04.self.py
+++ objects/04.self.py
@@ -3,3 +3,16 @@
 # palabra reservada class
 # 
 
+class Student:
+    def __init__(self, name, position, skills ):
+        self.name = name
+        self.position = position
+        self.skills = skills
+    def present_self(self):
+        print('My name is', self.name +',', "i'am a", self.position, 'nd my skills are', self.skills)
+
+
+alice_skills = ["Python", "Git", "HTML","CSS","Javascript"]
+alice = Student('Alice', 'Fullstack Developer', alice_skills )
+bob = Student('Bob', 'Chef', 'to cook ñami meals')
+alice.present_self()
